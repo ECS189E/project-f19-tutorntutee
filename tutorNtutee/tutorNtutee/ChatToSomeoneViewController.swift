aea28032833d82fd0100b0b1e256e55fd9853264
Junjie Han <jiehan@ucdavis.edu>
2019-11-21 11:25:13 -0800

    very beginning of chat

--- tutorNtutee/tutorNtutee/ChatToSomeoneViewController.swift
+++ tutorNtutee/tutorNtutee/ChatToSomeoneViewController.swift
@@ -7,24 +7,59 @@
 //
 
 import UIKit
+import Firebase
 
-class ChatToSomeoneViewController: UIViewController {
-
+class ChatToSomeoneViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    var sendId:String="0000"
+    var receiveId:String="1111"
+    var messageSendArray:[String]=[]
+    var messageReceiveArray:[String]=[]
+    var ref:DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let sender=Auth.auth().currentUser{
+            self.sendId=sender.uid
+        }
+        
+        self.ref=Database.database().reference()
+        let query=ref.child("chat").child(sendId).child(receiveId).queryLimited(toLast: 10)
+        _ = query.observe(.childAdded, with: { [weak self] snapshot in
+            if let data=snapshot.value as? [String:String],
+                let text=data["text"],
+                !text.isEmpty{
+                self?.messageReceiveArray.append(text)
+            }
+        })
+        
+        
+        self.tableView.delegate=self
+        self.tableView.dataSource=self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendMessage() {
+        if let text =  messageTextField.text{
+            messageSendArray.append(text)
+            tableView.reloadData()
+            messageTextField.text=""
+            self.ref.child("chat").child(sendId).child(receiveId).child("text").child(text)
+        }
     }
-    */
-
+    
+    //MARK: tableView Statement
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageSendArray.count+messageReceiveArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell1=UITableViewCell(style: .default, reuseIdentifier: "sendCell")
+        cell1.textLabel?.text=messageSendArray[indexPath.row]
+        return cell1
+    }
+    
 }
