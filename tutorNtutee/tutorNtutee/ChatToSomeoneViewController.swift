f96bc37b0ddf4b951dbd3f5d9e5626a88550ae80
Junjie Han <jiehan@ucdavis.edu>
2019-11-21 11:02:22 -0800

    beginning chat

---
+++ tutorNtutee/tutorNtutee/ChatToSomeoneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatToSomeoneViewController.swift
+//  tutorNtutee
+//
+//  Created by Junjie Han on 11/21/19.
+//  Copyright © 2019 Alessandro Liu. All rights reserved.
+//
+
+import UIKit
+
+class ChatToSomeoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
