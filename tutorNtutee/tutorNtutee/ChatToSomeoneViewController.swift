940936593a56b907c4417c3d7660934c0adfd404
Junjie Han <jiehan@ucdavis.edu>
2019-11-24 18:52:56 -0800

    chatImproved

--- tutorNtutee/tutorNtutee/ChatToSomeoneViewController.swift
+++ tutorNtutee/tutorNtutee/ChatToSomeoneViewController.swift
@@ -11,55 +11,70 @@ import Firebase
 
 class ChatToSomeoneViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
-    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     
-    var sendId:String="0000"
-    var receiveId:String="1111"
-    var messageSendArray:[String]=[]
-    var messageReceiveArray:[String]=[]
-    var ref:DatabaseReference!
+    var fromId:String? // = "0000"
+    var toId:String="qd1pYL2agYQB7b2TotHc9MbgtXC3"
+    var messageArray:[Message]=[]
+    //var ref:DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        messageTextField.placeholder="Enter message..."
+        
         if let sender=Auth.auth().currentUser{
-            self.sendId=sender.uid
+            self.fromId=sender.uid
         }
         
-        self.ref=Database.database().reference()
-        let query=ref.child("chat").child(sendId).child(receiveId).queryLimited(toLast: 10)
-        _ = query.observe(.childAdded, with: { [weak self] snapshot in
-            if let data=snapshot.value as? [String:String],
-                let text=data["text"],
-                !text.isEmpty{
-                self?.messageReceiveArray.append(text)
-            }
-        })
-        
+        observeMessage()
         
         self.tableView.delegate=self
         self.tableView.dataSource=self
     }
     
+    func observeMessage(){
+        let ref = Database.database().reference().child("messages")
+        ref.observe(.childAdded, with: { [weak self] snapshot in
+            if let dictionary = snapshot.value as? [String: Any]{
+                let message = Message(dictionary: dictionary)
+                if((message.fromId == self?.fromId && message.toId == self?.toId) || (message.toId == self?.fromId && message.fromId == self?.toId)){
+                    self?.messageArray.append(message)
+                    
+                    DispatchQueue.main.async(execute: {
+                        self?.tableView.reloadData()
+                    })
+                }
+            }
+        })
+    }
+    
     @IBAction func sendMessage() {
         if let text =  messageTextField.text{
-            messageSendArray.append(text)
-            tableView.reloadData()
+            let ref = Database.database().reference().child("messages")
+            let childRef = ref.childByAutoId()
+            let timestamp:NSNumber = NSNumber(value: NSDate().timeIntervalSince1970)
+            let values=["text":text,"fromId":fromId,"toId":toId,"timestamp":timestamp] as [String : Any]
+            childRef.updateChildValues(values)
             messageTextField.text=""
-            self.ref.child("chat").child(sendId).child(receiveId).child("text").child(text)
         }
     }
     
     //MARK: tableView Statement
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messageSendArray.count+messageReceiveArray.count
+        return messageArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell1=UITableViewCell(style: .default, reuseIdentifier: "sendCell")
-        cell1.textLabel?.text=messageSendArray[indexPath.row]
-        return cell1
+        let message = messageArray[indexPath.row]
+        var cell = tableView.dequeueReusableCell(withIdentifier: "receiveCell") ?? UITableViewCell(style: .default, reuseIdentifier: "receiveCell")
+        if(message.fromId == fromId){
+            cell = tableView.dequeueReusableCell(withIdentifier: "sendCell") ?? UITableViewCell(style: .default, reuseIdentifier: "sendCell")
+        }
+        cell.textLabel?.text=message.text
+        
+        return cell
     }
     
 }
---
+++ tutorNtutee/tutorNtutee/Message.swift
@@ -0,0 +1,23 @@
+//
+//  Message.swift
+//  tutorNtutee
+//
+//  Created by Junjie Han on 11/22/19.
+//  Copyright © 2019 Alessandro Liu. All rights reserved.
+//
+
+import UIKit
+
+class Message: NSObject {
+    var text:String?
+    var fromId:String?
+    var toId:String?
+    var timestamp: NSNumber?
+    
+    init(dictionary: [String: Any]){
+        self.fromId = dictionary["fromId"] as? String
+        self.toId = dictionary["toId"] as? String
+        self.text = dictionary["text"] as? String
+        self.timestamp = dictionary["timestamp"] as? NSNumber
+    }
+}
