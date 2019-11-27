de69370ce026782cc4a13f3e6751e2e252c328b7
Alessandro <alle95liu@gmail.com>
2019-11-26 20:19:50 -0800

    added global post child on firebase database

--- tutorNtutee/tutorNtutee/DateVC.swift
+++ tutorNtutee/tutorNtutee/DateVC.swift
@@ -22,106 +22,128 @@ class DateVC: UIViewController {
     private var timePicker : UIDatePicker?
     private var tilTimePicker : UIDatePicker?
     var ref: DatabaseReference!
+    var postsRef: DatabaseReference!
     var updateRef: DatabaseReference!
+    var updatePostsRef: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // get the current user id
-        let userID = Auth.auth().currentUser?.uid
-        // go to the node where class_time is stored
-        ref = Database.database().reference().child("user").child(userID ?? "error with userID").child("tutor_class_time")
-        updateRef = Database.database().reference().child("user").child(userID ?? "error with userID")
-        print("check userID -->", userID ?? "error in getting userID !!!")
-        
-        // Do any additional setup after loading the view.
-        classLabel.text = selectedClass
-        datePicker = UIDatePicker()
-        datePicker?.datePickerMode = .date
-        datePicker?.minimumDate = Date()
-        timePicker = UIDatePicker()
-        timePicker?.datePickerMode = .time
-        tilTimePicker = UIDatePicker()
-        tilTimePicker?.datePickerMode = .time
-        
-        timePicker?.addTarget(self, action: #selector(DateVC.timeChanged(timePicker:)), for: .valueChanged)
-        
-        tilTimePicker?.addTarget(self, action: #selector(DateVC.tilTimeChanged(tilTimePicker:)), for: .valueChanged)
-        
-        datePicker?.addTarget(self, action: #selector(DateVC.dateChanged(datePicker:)), for: .valueChanged)
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(DateVC.viewTapped(gestureRecognizer:)))
-        view.addGestureRecognizer(tapGesture)
-        timeTextField.inputView = timePicker
-        dateTextField.inputView = datePicker
-        tilTimeTextField.inputView = tilTimePicker
-    }
-    
-    @IBAction func doneBtn(_ sender: Any) {
-        guard let sTime = timeTextField.text, (timeTextField.text ?? "") != ""  else {
-            print("start time has an error")
-            return
-        }
-        guard let eTime = tilTimeTextField.text, (tilTimeTextField.text ?? "") != ""  else {
-            print("end time has an error")
-            return
-        }
-        guard let d = dateTextField.text, (dateTextField.text ?? "") != ""  else {
-            print("date has an error")
-            return
-        }
-        guard let p = priceTextField.text, (priceTextField.text ?? "") != ""  else {
-            print("price has an error")
-            return
-        }
-        let startTime = sTime.replacingOccurrences(of: " ", with: "")
-        let endTime = eTime.replacingOccurrences(of: " ", with: "")
-        let myDate = d.replacingOccurrences(of: " ", with: "")
-        let myPrice = p.replacingOccurrences(of: " ", with: "")
-        
-        let newSchedule = "\(selectedClass) \(myDate) \(startTime) \(endTime) $\(myPrice)"
-        
-        print(newSchedule)
-        
-        var scheduleArray = [String]()
-        ref.observeSingleEvent(of: .value, with: { (snapshot) in
-            
-            
-            // Get user value
-
-//            let value = snapshot.value as? NSDictionary
-            let scheduleDB = snapshot.value as? NSArray
-//            guard let val = snapshot.value else {
-//                print("error in getting snapshot.value")
-//                return
-//            }
-            
-            for oneSchedule in scheduleDB ?? ["error in getting the nsarray elements"] {
-                print("----------------------------")
+                let userID = Auth.auth().currentUser?.uid
+                updatePostsRef=Database.database().reference()
+                postsRef = Database.database().reference().child("posts")
+                // go to the node where class_time is stored
+                ref = Database.database().reference().child("user").child(userID ?? "error with userID").child("tutor_class_time")
+                updateRef = Database.database().reference().child("user").child(userID ?? "error with userID")
+                print("check userID -->", userID ?? "error in getting userID !!!")
                 
-                let exisitingSchedule = oneSchedule as! String
-        
-                // check if the chedule already exist in the databse
-                if newSchedule.isEqual(exisitingSchedule) {
-                    print("the schedule already exists in the database")
-                    let errorMessage=UIAlertController(title: "error", message: "the schedule already exists in the database", preferredStyle: .alert)
-                    let close = UIAlertAction(title:"close",style: .cancel,handler: nil)
-                    errorMessage.addAction(close)
-                    self.present(errorMessage,animated: true,completion: nil)
+                // Do any additional setup after loading the view.
+                classLabel.text = selectedClass
+                datePicker = UIDatePicker()
+                datePicker?.datePickerMode = .date
+                datePicker?.minimumDate = Date()
+                timePicker = UIDatePicker()
+                timePicker?.datePickerMode = .time
+                tilTimePicker = UIDatePicker()
+                tilTimePicker?.datePickerMode = .time
+                
+                timePicker?.addTarget(self, action: #selector(DateVC.timeChanged(timePicker:)), for: .valueChanged)
+                
+                tilTimePicker?.addTarget(self, action: #selector(DateVC.tilTimeChanged(tilTimePicker:)), for: .valueChanged)
+                
+                datePicker?.addTarget(self, action: #selector(DateVC.dateChanged(datePicker:)), for: .valueChanged)
+                
+                let tapGesture = UITapGestureRecognizer(target: self, action: #selector(DateVC.viewTapped(gestureRecognizer:)))
+                view.addGestureRecognizer(tapGesture)
+                timeTextField.inputView = timePicker
+                dateTextField.inputView = datePicker
+                tilTimeTextField.inputView = tilTimePicker
+            }
+            
+            @IBAction func doneBtn(_ sender: Any) {
+                guard let sTime = timeTextField.text, (timeTextField.text ?? "") != ""  else {
+                    print("start time has an error")
                     return
                 }
-                print(exisitingSchedule)
-                scheduleArray.append(exisitingSchedule)
-            }
-            scheduleArray.append(newSchedule)
-            self.updateRef.updateChildValues(["tutor_class_time":scheduleArray])
-            
-//          let sss = scheduleDB?[0] as? String ?? "error in the class"
-//          print("sss -->", sss)
+                guard let eTime = tilTimeTextField.text, (tilTimeTextField.text ?? "") != ""  else {
+                    print("end time has an error")
+                    return
+                }
+                guard let d = dateTextField.text, (dateTextField.text ?? "") != ""  else {
+                    print("date has an error")
+                    return
+                }
+                guard let p = priceTextField.text, (priceTextField.text ?? "") != ""  else {
+                    print("price has an error")
+                    return
+                }
+                let startTime = sTime.replacingOccurrences(of: " ", with: "")
+                let endTime = eTime.replacingOccurrences(of: " ", with: "")
+                let myDate = d.replacingOccurrences(of: " ", with: "")
+                let myPrice = p.replacingOccurrences(of: " ", with: "")
+                
+                let newSchedule = "\(selectedClass) \(myDate) \(startTime) \(endTime) $\(myPrice)"
+                
+                print(newSchedule)
+                
+                var scheduleArray = [String]()
+                ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                    
+                    
+                    // Get user value
 
-          }) { (error) in
-            print(error.localizedDescription)
-        }
+        //            let value = snapshot.value as? NSDictionary
+                    let scheduleDB = snapshot.value as? NSArray
+        //            guard let val = snapshot.value else {
+        //                print("error in getting snapshot.value")
+        //                return
+        //            }
+                    
+                    for oneSchedule in scheduleDB ?? ["error in getting the nsarray elements"] {
+                        print("----------------------------")
+                        
+                        let exisitingSchedule = oneSchedule as! String
+                
+                        // check if the chedule already exist in the databse
+                        if newSchedule.isEqual(exisitingSchedule) {
+                            print("the schedule already exists in the database")
+                            let errorMessage=UIAlertController(title: "error", message: "the schedule already exists in the database", preferredStyle: .alert)
+                            let close = UIAlertAction(title:"close",style: .cancel,handler: nil)
+                            errorMessage.addAction(close)
+                            self.present(errorMessage,animated: true,completion: nil)
+                            return
+                        }
+                        print(exisitingSchedule)
+                        scheduleArray.append(exisitingSchedule)
+                        
+                    }
+                    scheduleArray.append(newSchedule)
+                    self.updateRef.updateChildValues(["tutor_class_time":scheduleArray])
+                    
+        //          let sss = scheduleDB?[0] as? String ?? "error in the class"
+        //          print("sss -->", sss)
+
+                  }) { (error) in
+                    print(error.localizedDescription)
+                }
+                
+                var newPostsArray = [String]()
+                postsRef.observeSingleEvent(of: .value, with: { (snapshot) in
+                    if (snapshot.exists()){
+                        let postsArray = snapshot.value as? NSArray
+                        print("exists!", postsArray ?? ["error in parsing posts"])
+                        for onePost in  postsArray ?? ["error in parsing posts"] {
+                            let exisitingPost = onePost as! String
+                            newPostsArray.append(exisitingPost)
+                        }
+                        newPostsArray.append(newSchedule)
+                        self.updatePostsRef.updateChildValues(["posts":newPostsArray])
+                        
+                    }
+                }) { (error) in
+                        print(error.localizedDescription)
+                }
 
     }
     
