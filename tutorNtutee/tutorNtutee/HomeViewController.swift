4def0de49823397dc1b67a48b2a6a70b284ac784
aadila6 <mbgoh@ucdavis.edu>
2019-12-04 11:54:17 -0800

    Parsing post done!

--- tutorNtutee/tutorNtutee/HomeViewController.swift
+++ tutorNtutee/tutorNtutee/HomeViewController.swift
@@ -53,7 +53,7 @@ class HomeViewController: UIViewController,UITableViewDataSource, UITableViewDel
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.postArray.count-1
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -62,8 +62,7 @@ class HomeViewController: UIViewController,UITableViewDataSource, UITableViewDel
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reusecell", for: indexPath) as! SessionPostCell
-        cell.courseName.text = "ECS 150"
-        cell.avaliable.text = "Nov 25, 3-5pm"
+        
         cell.myimage.image = UIImage(named: "user.png")
         cell.backgroundImg.layer.masksToBounds = true
         cell.backgroundImg.layer.cornerRadius = 10.0
@@ -72,25 +71,24 @@ class HomeViewController: UIViewController,UITableViewDataSource, UITableViewDel
 //            print(self.postArray[n])
 //        }
         
-
-//        let fullNameArr : [String] = fullName.components(separatedBy: " ")
-//        let className : String = fullNameArr[0]
-//        let date : String = fullNameArr[1]
-//        let timeS : String = fullNameArr[2]
-//        let timeE : String = fullNameArr[3]
-//        let cost : String = fullNameArr[4]s
-//        cell.courseName.text = self.postArray[indexPath.row + 1]
-//        cell.avaliable.text = "time"
-        //            cell.courseName.text = className
-        //            cell.avaliable.text = "\(date) \(timeS) to \(timeE)"
-        //            cell.textLabel?.text = "\(indexPath.row)"
-        //            cell.backgroundColor = UIColor.white
+        let fullName : String = self.postArray[indexPath.row+1]
+        print(fullName)
+        let fullNameArr : [String] = fullName.components(separatedBy: " ")
+        let className : String = fullNameArr[0]
+        let date : String = fullNameArr[1]
+        let timeS : String = fullNameArr[2]
+        let timeE : String = fullNameArr[3]
+        let cost : String = fullNameArr[4]
+        cell.courseName.text = className
+        cell.avaliable.text = "\(date) \(timeS) to \(timeE)"
+                    
+        cell.textLabel?.text = "\(indexPath.row)"
+        cell.backgroundColor = UIColor.white
         cell.layer.borderColor = UIColor.black.cgColor
         cell.layer.borderWidth = 1
         cell.layer.cornerRadius = 30
         
-//        let fullName : String = self.postArray[indexPath.row]
-//        print(fullName)
+
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
